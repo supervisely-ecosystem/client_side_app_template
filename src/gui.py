03b7fc3515c3c5955c2b961436ad42606b18c549
Nikolai Peukhov <petukhov.niko@gmail.com>
2024-12-17 10:41:11 -0300

    update

--- src/gui.py
+++ src/gui.py
@@ -1,12 +1,15 @@
 from supervisely.app.widgets import Text, Container, Button, Select
 
-text = Text("Hello, World!", widget_id="widget_1")
-select = Select(
-    items=[Select.Item("Option 1"), Select.Item("Option 2"), Select.Item("Option 3")],
-    widget_id="widget_2",
+text: Text = globals().get("text", Text("Hello, World!", widget_id="widget_1"))
+select: Select = globals().get(
+    "select",
+    Select(
+        items=[Select.Item("Option 1"), Select.Item("Option 2"), Select.Item("Option 3")],
+        widget_id="widget_2",
+    ),
 )
-button = Button("Click me!", widget_id="widget_3")
-layout = Container(widgets=[text, select, button], widget_id="widget_4")
+button = globals().get("button", Button("Click me!", widget_id="widget_3"))
+layout = globals().get("layout", Container(widgets=[text, select, button], widget_id="widget_4"))
 
 
 @select.value_changed
@@ -17,3 +20,4 @@ def on_select_change(value):
 @button.click
 def on_button_click():
     print("Button clicked!")
+    text.text = str(select.get_value())
